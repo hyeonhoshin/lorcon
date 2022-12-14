12e0ba3e3ea79a17b52e398888f028809341126a
Hyeonho Shin <35714982+hyeonhoshin@users.noreply.github.com>
2022-12-14 10:15:44 +0900

    Debug. Restoration from SDR-Lite based injector.
    SDR-Lite based injector file transmits redundant packets, so that we cannot track the right scrambling seed.

--- tools/injector.c
+++ tools/injector.c
@@ -1,19 +1,32 @@
 /*
-	beacon_flood_raw.c 
-	by brad.antoniewicz@foundstone.com	
+    Generates data frames at every MCS value across 20 and 40mhz
+    for testing tx and rx.
 
-	simple IEEE 802.11 beacon flooder using LORCON2's 
-	raw sending capabilities
+    Allows tagging packets with location info for site surveys.
 
+    Packets are sent as beacons encoded at different MCS rates; 
+    beacons SHOULD bypass the triggering of CTS/RTS frame control.
+
+    Packets are sent with 2 custom IE tags:
+
+    ie 10, length 14
+        Packed byte field encoding:
+
+            Byte 0 - MCS rate and flags, where
+                Bit 7 indicates HT40 mode
+                Bit 6 indicates ShortGI mode
+                Bits 0-3 indicate MCS values 0-15
+            Byte 1 - Location code (0-255)
+            Bytes 2-5
+                32 bit current packet count
+            Bytes 6-9
+                32 bit maximum packet count
+            Bytes 10-13
+                32 bit random session id
+
+    ie 11, length 64
+        Text field containing a text description of the packed field
 */
-#include <stdlib.h>
-#include <stdio.h>
-#include <getopt.h>
-#include <lorcon2/lorcon.h> // For LORCON 
-
-
-
-
 
 #include <stdio.h>
 #include <getopt.h>
@@ -25,154 +38,318 @@
 #include <lorcon2/lorcon_forge.h>
 #include <lorcon2/lorcon_packasm.h>
 
+/* MCS only goes 0-15 or 4 bits, so we use bits 6 and 7 to indicate if we
+ * are sending HT40 and GI */
+#define HT_FLAG_40  (1 << 7)
+#define HT_FLAG_GI  (1 << 6)
 
-
+#define PAYLOAD_LEN 1571
 
 void usage(char *argv[]) {
-	printf("\t-i <int> \tInterface\n");
-	printf("\t-c <channel>\tChannel\n");
-	printf("\t-l <int> \tlength\n");
-	printf("\nExample:\n");
-	printf("\t%s -i wlan0 -c 11HT20 -l 900\n\n",argv[0]);
+    printf("\t-i <interface>        Radio interface\n");
+    printf("\t-c <channel>          Channel (should be HT40)\n");
+    printf("\t-m <MCS_index>        MCS index (0~15)\n");
+    printf("\t-b <band_width>       Band width(0-20M | 1-40M)\n");
+    printf("\t-g <guard_interval>   Guard interval\n");
+    printf("\t-n <count>            Number of packets at each MCS to send.(Should be 128n)\n");
+    printf("\t-d <delay>            Interframe delay\n");
+    printf("\t-l <length>           PSDU length\n");
+
+    printf("\nExample:\n");
+    printf("\t%s -i wlan0 -c 6HT40+ -m 0 -b 0 -g 0 -n 256 -l 2080\n\n", argv[0]);
 }
 int main(int argc, char *argv[]) {
+    char *interface = NULL;
+    unsigned int lcode = 0;
+    unsigned int npackets = 100;
+    unsigned int MCS = 0;
+    unsigned int length = 2080;
 
-	printf("============= Tanuki Injector v0.0 ==============\n");
+    int value[6];
+    int c,i,tmp;
+    int channel, ch_flags;
 
-	char *interface = NULL, *ssid = NULL;
-	int c;
-	unsigned int count=0;
-	int length=5000;
-	lorcon_driver_t *drvlist, *driver; // Needed to set up interface/context
-	lorcon_t *context; // LORCON context
+    lorcon_driver_t *drvlist, *driver;
+    lorcon_t *context;
 
- 	//int interval = 5;
+    lcpa_metapack_t *metapack;
+    lorcon_packet_t *txpack;
 
-	int channel, ch_flags;
-   int num;
-    char current;
-    int rr;
-   FILE *fptr;
+    /* delay interval */
+    unsigned int interval = 1;
 
-	/* 
-		This handles all of the command line arguments
-	*/
+    /* Iterations through HT and GI */
+//    int mcs_iter = 0;
+    int BW = 0;
+    int GI = 0; 
+//    int ht_iter = 0;
+//    int gi_iter = 0;
+    unsigned int count = 0;
+
+    unsigned int totalcount = 1;
+
+    uint8_t *dmac = "\x04\xF0\x21\x32\xBD\xA5";
+
+    uint8_t *bmac = "\x00\xDE\xAD\xBE\xEF\x00";
+
+//    uint8_t *RA_MAC = "\x04\xF0\x21\x32\xBD\xA5";
+    uint8_t RA_MAC[6];
+    RA_MAC[0] = 0x04;
+    RA_MAC[1] =0xF0;
+    RA_MAC[2] =0x21;
+    RA_MAC[3] =0x32;
+    RA_MAC[4] =0xBD;
+    RA_MAC[5] =0xA5;
+    uint8_t *TA_MAC;
+    uint8_t *DA_MAC = RA_MAC;
+    uint8_t *BSSID_MAC = bmac;
+
+    uint8_t fcflags = 3;
+    uint8_t fragement = 3;
+    uint8_t sequence = 2;
+    unsigned int duration = 0;
+    uint8_t encoded_payload[14];
+    uint32_t *encoded_counter = (uint32_t *) (encoded_payload + 2);
+    uint32_t *encoded_max = (uint32_t *) (encoded_payload + 6);
+    uint32_t *encoded_session = (uint32_t *) (encoded_payload + 10);
+
+    // Timestamp
+    struct timeval time; 
+    uint64_t timestamp; 
+
+    // Beacon Interval
+    int beacon_interval = 100;
+
+    // Capabilities
+    int capabilities = 0x0421;
+
+    // Session ID
+    uint32_t session_id;
+    FILE *urandom;
+
+    printf ("%s - packet injector tanuki!!\n", argv[0]);
+    printf ("-----------------------------------------------------\n\n");
+
+    while ((c = getopt(argc, argv, "hi:c:m:b:g:n:d:a:l:")) != EOF) {
+	switch (c) {
+	case 'i': 
+		interface = strdup(optarg);
+		break;
+	case 'c':
+		if (lorcon_parse_ht_channel(optarg, &channel, &ch_flags) == 0) {
+	    		printf("ERROR: Unable to parse channel\n");
+	    		return -1;
+		}
+		break;
+	case 'm':
+		if (sscanf(optarg, "%u", &MCS) != 1){
+		    printf("ERROR: Unable to parse MCS idex\n");
+		    return -1;
+		}
+		break;
+    	case 'b':
+		if (sscanf(optarg, "%u", &BW) != 1){
+		    printf("ERROR: Unable to parse bandwidth \n");
+		    return -1;
+		}
+		break;
+	case 'g':
+		if (sscanf(optarg, "%u", &GI) != 1){
+		    printf("ERROR: Unable to parse guard interval \n");
+		    return -1;
+		}
+		break;
+    	case 'n':
+		if (sscanf(optarg, "%u", &npackets) != 1) {
+		    printf("ERROR: Unable to parse number of packets\n");
+		    return -1;
+		}
+		break;
+
+    	case 'd':
+		if (sscanf(optarg, "%u", &interval) != 1) {
+		    printf("ERROR: Unable to parse interframe interval\n");
+		    return -1;
+		}
+		break;
+	case 'a':
+		//if (6 == sscanf(optarg, "%x:%x:%x:%x:%x:%x", &value[0],&value[1],&value[2],&value[3],&value[4],&value[5]) ){
+		tmp = sscanf(optarg, "%x:%x:%x:%x:%x:%x", &value[0],&value[1],&value[2],&value[3],&value[4],&value[5]);
+		
+		printf("Read MAC, num:%d\n",tmp);
+		if (6 == tmp ){
+			printf("Read MAC, entering loop\n");
+			for(i = 0; i < 6; i++){
+				printf("Read MAC, loop index:%d\n",i);
+				RA_MAC[i] = (uint8_t)value[i];	
+			}
+		}else{
+		    printf("ERROR: Unable to parse MAC address\n");
+		    return -1;
+		}
+		break;	
+	case 'h':
+		printf("ERROR: cannot parse the input\n");
+		usage(argv);
+		return -1;
+		break;
+	case 'l':
+		if (sscanf(optarg, "%u", &length) != 1) {
+		    printf("ERROR: Unable to parse packet length\n");
+		    return -1;
+		}
+		break;
+	default:
+		usage(argv);
+		return -1;
+		break;
+	}
+    }
+
+    if ( interface == NULL) { 
+	printf ("ERROR: Interface, or channel not set (see injector -h for more info)\n");
+	return -1;
+    }
+
+    if ((urandom = fopen("/dev/urandom", "rb")) == NULL) {
+        printf("ERROR:  Could not open urandom for session id: %s\n", strerror(errno));
+        return -1;
+    }
+
+    fread(&session_id, 4, 1, urandom);
+    fclose(urandom);
+
+    printf("[+] Using interface %s\n",interface);
 	
-	while ((c = getopt(argc, argv, "hi:c:l:")) != EOF) {
-		switch (c) {
-			case 'i': 
-				interface = strdup(optarg);
-				break;
-			case 'c':
-				if (lorcon_parse_ht_channel(optarg, &channel, &ch_flags) == 0) {
-			    		printf("ERROR: Unable to parse channel\n");
-			    		return -1;
-				}
-				break;
+    if ((driver = lorcon_auto_driver(interface)) == NULL) {
+	printf("[!] Could not determine the driver for %s\n", interface);
+	return -1;
+    } else {
+	printf("[+]\t Driver: %s\n",driver->name);
+    }
 
-			case 'l':
-				//length = atoi(optarg);
-				sscanf(optarg, "%d", &length);
-				break;
-			case 'h':
-				usage(argv);
-				break;
-			default:
-				usage(argv);
-				break;
-			}
-	}
+    if ((context = lorcon_create(interface, driver)) == NULL) {
+        printf("[!]\t Failed to create context");
+        return -1; 
+    }
 
-	if ( interface == NULL  ) { 
-		printf ("ERROR: Interface not set (see -h for more info)\n");
-		return -1;
-	}
+    // Create Monitor Mode Interface
+    if (lorcon_open_injmon(context) < 0) {
+	printf("[!]\t Could not create Monitor Mode interface!\n");
+	return -1;
+    } else {
+	printf("[+]\t Monitor Mode VAP: %s\n\n",lorcon_get_vap(context));
+	lorcon_free_driver_list(driver);
+    }
 
-	printf("[+] Using interface %s\n Inject 20MHz packets.\n",interface);
-				printf("\n length is %d\n",length);
-	uint8_t packet[5000] = {0x00};
-	char fname_full[200] = {'\0'};
+    // Get the MAC of the radio
+    if (lorcon_get_hwmac(context, &TA_MAC) <= 0) {
+        printf("[!]\t Could not get hw mac address\n");
+        return -1;
+    }
 
+    printf("[+]\t Using MAC: %02x:%02x:%02x:%02x:%02x:%02x \n",TA_MAC[0],TA_MAC[1],TA_MAC[2],TA_MAC[3],TA_MAC[4],TA_MAC[5]);
+    printf("[+]\t RX MAC: %02x:%02x:%02x:%02x:%02x:%02x \n",RA_MAC[0],RA_MAC[1],RA_MAC[2],RA_MAC[3],RA_MAC[4],RA_MAC[5]);
+    // Set the channel we'll be injecting on
+    lorcon_set_ht_channel(context, channel, ch_flags);
 
-	if ( (driver = lorcon_auto_driver(interface)) == NULL) {
-		printf("[!] Could not determine the driver for %s\n",interface);
-		return -1;
-	} else {
-		printf("[+]\t Driver: %s\n",driver->name);
-	}
+    printf("[+]\t Using channel: %d flags %d\n", channel, ch_flags);
+    printf("[+]\t Packet length: %d\n",length);
 
-	// Create LORCON context
-        if ((context = lorcon_create(interface, driver)) == NULL) {
-                printf("[!]\t Failed to create context");
-               	return -1; 
+    
+    printf("\n[.]\tMCS %u %s %s\n\n", MCS, BW ? "40MHz" : "20MHz", GI ? "short-gi" : "long-gi");
+    
+    uint8_t scrambling_seed = 71;
+    int rr;
+    FILE *fptr;
+    char filename[10];
+	
+    //uint8_t *payload = (uint8_t*)malloc(length);
+    //if(payload == NULL){
+    //    printf("[!] Malloc Error!\n");
+    //	exit(-1);
+    //}
+	
+    uint8_t payload[5000] = {'\0'};
+
+    for (count = 0; count < npackets; count++) {
+        memset(payload, 0, length);
+	
+	sprintf(filename,"ctc/genpkt_%03d.txt", scrambling_seed);
+	    
+        if((fptr = fopen(filename,"r"))==NULL){
+                printf("Error! opening file");
+                exit(1);
         }
+        // Payload changing.
+        for (i = 0; i < length; i++){
+            //payload[2*i] = count & 0x00FF;
+            //payload[2*i+1] = (count & 0xFF00) >> 8;
+            fscanf(fptr,"%d\n", &rr);
+            payload[i]=(uint8_t)rr;
 
-	// Create Monitor Mode Interface
-	if (lorcon_open_injmon(context) < 0) {
-		printf("[!]\t Could not create Monitor Mode interface!\n");
-		return -1;
-	} else {
-		printf("[+]\t Monitor Mode VAP: %s\n",lorcon_get_vap(context));
-		lorcon_free_driver_list(driver);
-	}
+            //printf("%d ",i);
+        }
+        //printf("\n");
+
+        memset(encoded_payload, 0, 14);
+
+        // Set MCS count
+        encoded_payload[0] = MCS;
+        if (GI)
+            encoded_payload[0] |= HT_FLAG_GI;
+        if (BW)
+            encoded_payload[0] |= HT_FLAG_40;
+
+        // set the location code
+        encoded_payload[1] = lcode & 0xFF;
+
+        *encoded_counter = htonl(count);
+        *encoded_max = htonl(npackets);
+        *encoded_session = htonl(session_id);
+
+        metapack = lcpa_init();
+
+        // Create timestamp
+        gettimeofday(&time, NULL);
+        timestamp = time.tv_sec * 1000000 + time.tv_usec;
+
+        lcpf_data(metapack,fcflags,duration,RA_MAC,TA_MAC,RA_MAC,TA_MAC,fragement,sequence);
 
 
+        //lcpf_add_ie(metapack, 0, strlen("Packet_Injection"), "Packet_Injection");
+        //lcpf_add_ie(metapack, 10, 14, encoded_payload);
+        //lcpf_add_ie(metapack, 11, PAYLOAD_LEN, payload);
+        lcpa_append_copy(metapack, "IETAG", length, payload); // Using lcpa lib.
+        //lcpf_add_ie(metapack, 12, strlen((char *) payload_1), payload_1);
 
-	lorcon_set_ht_channel(context, channel, ch_flags);
 
-	printf("[+]\t Using channel: %d\n\n",channel);
+        // Convert the LORCON metapack to a LORCON packet for sending
+        txpack = (lorcon_packet_t *) lorcon_packet_from_lcpa(context, metapack);
 
-	printf("[+]\t length is: %d\n\n",length);
+        lorcon_packet_set_mcs(txpack, 1, MCS, GI, BW);
+		
+        if (lorcon_inject(context,txpack) < 0 ) 
+            return -1;
 
-	char seq[2]="0";
+        usleep(interval * 1000);
 
-	int file_seq=0;
-	int rrr=0;
+        printf("\033[K\r");
+        printf("[+] Sent %d frames, Scrambling seed : %d, Hit CTRL + C to stop...", totalcount, scrambling_seed);
+        fflush(stdout);
+	   
+        totalcount++;
+	scrambling_seed = scrambling_seed + 1;
+	if(scrambling_seed == 128){scrambling_seed=1;}
 
-///////   send out 6*127 copies of "Input_Bytes.txt"
-	for (rrr=0;rrr<6;rrr++){ // Repeat 6 times.
-
-	    for (file_seq=0;file_seq<127;file_seq++){
-
-			sprintf(fname_full, "gen_pkt_%d.txt",file_seq); // The number indicates scrambling seed.
-		    	printf("Reading : %s",fname_full);
-		    	fflush(stdout);
-
-/////////      read file Input_Bytes.txt
-			if ((fptr = fopen(fname_full,"r")) == NULL){
-				printf("Error! opening file\n");
-				printf("fptr = %x\n",fptr);
-
-				// Program exits if the file pointer returns NULL.
-				exit(1);
-			}
-
-			for (num=0;num<length;num++){
-				fscanf(fptr,"%d\n", &rr);
-				packet[num]=(uint8_t)rr;
-			}
-			//printf("[+]\t length is: %d\n\n",length);
-			if ( lorcon_send_bytes(context, sizeof(char)*length, packet) < 0 ) 
-				return -1;
-
-			// Wait interval before next beacon
-				usleep(1000);
-
-			// Print nice and pretty
-			printf("\033[K\r");
-			printf("[+] Sent %d frames, Hit CTRL + C to stop...", count+1);
-			fflush(stdout);
-			count++;
-	    }
-	}
+        lcpa_free(metapack); 
+    }
 
     printf("\n");
-	// Close the interface
-	lorcon_close(context);
-
-	// Free the LORCON Context
-	lorcon_free(context);	
+    // Close the interface
+    lorcon_close(context);
+    // Free the LORCON Context
+    lorcon_free(context);	
 	
-	return 0;
+    return 0;
 }
-
